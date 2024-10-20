38da7316c9cc50ae9782e85842f44f8ffa8f9237
darko-mesaros <darko.subotica@gmail.com>
2024-10-19 22:20:45 -0700

    Initial commit

---
+++ test/shuk_file_handler.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShukFileHandler from '../lib/shuk_file_handler-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shuk_file_handler-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShukFileHandler.ShukFileHandlerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
